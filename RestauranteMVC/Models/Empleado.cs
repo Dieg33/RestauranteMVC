c8f72aa8e5d8c146fae4a459523ff801e266a73a
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-03-31 17:40:19 -0600

    modelos creados

---
+++ RestauranteMVC/Models/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace RestauranteMVC.Models
+{
+    public class Empleado
+    {
+        public int EmpleadoId { get; set; }
+
+        public string urlFotoEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+
+        public string contrasenia { get; set; }
+        public int RolID { get; set; }
+        public DateTime FechaIngreso {get; set;}
+
+    }
+}
