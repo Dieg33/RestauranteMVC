5041f822d54868d39b555df567754ec3bfa38dce
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-02 10:45:50 -0600

    corregido para que acepte null

--- RestauranteMVC/Models/Empleado.cs
+++ RestauranteMVC/Models/Empleado.cs
@@ -4,17 +4,18 @@ namespace RestauranteMVC.Models
 {
     public class Empleado
     {
-        public int EmpleadoId { get; set; }
-        public string urlFotoEmpleado { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Email { get; set; }
-        public string Telefono { get; set; }
+        
+            public int EmpleadoId { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public string Email { get; set; }
+            public string Contrasena { get; set; }
+            public int RolID { get; set; }
+            public string? Telefono { get; set; }  
+            public DateTime? FechaIngreso { get; set; }  
+            public string? urlFotoEmpleado { get; set; }  
+        
 
-        public string Contrasena { get; set; }
-    
-        public int RolID { get; set; }
-        public DateTime FechaIngreso {get; set;}
 
     }
 }
