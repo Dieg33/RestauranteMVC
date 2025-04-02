c4aeabb5f8dbb214b4a918b296d2087030a43bc1
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-01 20:53:37 -0600

    carpte y clase realizada

---
+++ RestauranteMVC/Servicios/AutenticacionAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RestauranteMVC.Servicios
+{
+    public class AutenticacionAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var session = context.HttpContext.Session.GetInt32("EmpleadoID");
+            if (session == null)
+            {
+                context.Result = new RedirectToActionResult("Autenticar", "Home", null);
+            }
+            base.OnActionExecuting(context);
+        }
+
+    }
+}
