2889635d1e629696cf20e0006c0a00fe195d772c
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-01 20:52:36 -0600

    controlador terminado

--- RestauranteMVC/Controllers/HomeController.cs
+++ RestauranteMVC/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RestauranteMVC.Models;
+using RestauranteMVC.Servicios;
+
 
 namespace RestauranteMVC.Controllers
 {
@@ -16,8 +18,17 @@ namespace RestauranteMVC.Controllers
 
         }
 
+        [Autenticacion]
         public IActionResult Index()
         {
+            var EmpleadoID = HttpContext.Session.GetInt32("EmpleadoID");
+            var Nombre = HttpContext.Session.GetString("Nombre");
+            if (EmpleadoID == null)
+            {
+                return RedirectToAction("Autenticar");
+            }
+            ViewBag.nombre = HttpContext.Session.GetString("Nombre");
+            ViewData["Apellido"] = Nombre;
             return View();
         }
 
@@ -31,5 +42,35 @@ namespace RestauranteMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public async Task<IActionResult> Autenticar(string txtEmail, string txtClave)
+        {
+            var empleado = (from e in _context.Empleados
+                            where e.Email == txtEmail && e.contrasenia == txtClave && e.RolID != 0
+                            select e).FirstOrDefault();
+            if (empleado != null)
+            {
+                HttpContext.Session.SetInt32("EmpleadoId", empleado.EmpleadoId);
+                HttpContext.Session.SetString("RolID", empleado.RolID.ToString());
+                HttpContext.Session.SetString("Nombre", empleado.Nombre);
+                return RedirectToAction("Admin", "Home");
+            }
+            ViewData["ErrorMessage"] = "Error, empleado inválido!";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Autenticar()
+        {
+            ViewData["ErrorMessage"] = "";
+            return View();
+        }
+
     }
 }
