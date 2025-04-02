a7e269531d72486cd814a77ca44619e7389dac80
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-01 20:52:49 -0600

    terminado

--- RestauranteMVC/Models/Empleado.cs
+++ RestauranteMVC/Models/Empleado.cs
@@ -5,7 +5,6 @@ namespace RestauranteMVC.Models
     public class Empleado
     {
         public int EmpleadoId { get; set; }
-
         public string urlFotoEmpleado { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
@@ -13,6 +12,7 @@ namespace RestauranteMVC.Models
         public string Telefono { get; set; }
 
         public string contrasenia { get; set; }
+    
         public int RolID { get; set; }
         public DateTime FechaIngreso {get; set;}
 
