6d865d88a4a6756d4d8f731fd83e2631335139b0
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-06 18:14:31 -0600

    Dbset  creados

--- RestauranteMVC/Models/RestauranteDbContext.cs
+++ RestauranteMVC/Models/RestauranteDbContext.cs
@@ -11,5 +11,14 @@ namespace RestauranteMVC.Models
 
         public DbSet<Empleado> Empleados { get; set; }
         public DbSet<Rol> Rol { get; set; }
+        public DbSet<Platos> Platos { get; set; }
+        public DbSet<Menu> Menu { get; set; }
+        public DbSet <Promociones> Promociones { get; set; }
+        public DbSet<PlatosCombos> PlatosCombos { get;set; }
+        public DbSet<Mesa> Mesa { get; set; }
+        public DbSet<MetodosPago> MetodosPagos { get; set; }
+        public DbSet<Combos> Combos {  get; set; }
+        public DbSet <Categorias>Categorias { get; set; }
+
     }
 }
