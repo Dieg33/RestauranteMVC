7e4204361bcc4860674b76241f54e4feaa2c771d
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-04-06 21:51:00 -0600

    models terminado

---
+++ RestauranteMVC/Models/Menu_Items.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestauranteMVC.Models
+{
+    public class Menu_Items
+    {
+        [Key]
+
+        public int MenuItemId { get; set; }
+        public int MenuID { get; set; }
+        public int PlatoID { get; set; }
+        public int ComboID { get; set; }
+        public int PromocionID { get; set; }
+    }
+}
---
+++ RestauranteMVC/Models/Promociones_Items.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestauranteMVC.Models
+{
+    public class Promociones_Items
+    {
+        [Key]
+        public int PromocionID { get; set; }
+        public int PlatoID { get; set; }
+        public int ComboID { get; set; }
+    }
+}
